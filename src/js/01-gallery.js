aa760f6aece6eaadaf55a4b45c53dd00722e5c33
Olesia-Seleznova <106627666+Olesia-Seleznova@users.noreply.github.com>
2022-10-22 21:05:02 +0300

    add 01-galery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onGalleryPictureClick);
+
+function createGalleryItemMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+      `;
+        })
+        .join('');
+};
+
+function onGalleryPictureClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    };
+    const galleryElement = event.target.dataset.source;
+    const instance = basicLightbox.create(`
+	        <img
+              src="${galleryElement}"
+              width = "800"
+              height = "600"/>
+    `);
+    instance.show();
+};
